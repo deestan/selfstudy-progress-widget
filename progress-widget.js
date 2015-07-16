c4f6bf8bdeaf0aa5dfd92b32a20dcdb67d064831
Helge Skogly Holm <helge.holm@gmail.com>
2015-07-16 19:13:38 +0200

    Added some style and basic animation.

--- progress-widget.js
+++ progress-widget.js
@@ -33,7 +33,7 @@ function insertBlobs(blobs, numTiers, parentId) {
     return ((genXseed++ + 1) * insideWidth) / (xPositions - 1);
   }
 
-  blobObjects = [];
+  blobAnimations = [];
   for (var i=0; i < blobs.length; i++) {
     var blob = blobs[i];
     var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
@@ -42,23 +42,37 @@ function insertBlobs(blobs, numTiers, parentId) {
     circle.setAttribute("cy", cy);
     circle.setAttribute("r", 5 * (numTiers - blob.maxTier + 1));
     circle.setAttribute("fill", "#ffffff");
+    var animation = document.createElementNS("http://www.w3.org/2000/svg", "animate");
+    animation.setAttribute("attributeName", "cy");
+    animation.setAttribute("from", cy);
+    animation.setAttribute("to", cy);
+    animation.setAttribute("begin", "indefinite");
+    animation.setAttribute("dur", "1s");
+    //animation.setAttribute("keySplines", "0 1 1 0");
+    //animation.setAttribute("keyTimes", "0 1");
+    circle.appendChild(animation);
+    blobAnimations.push(animation);
     parent.appendChild(circle);
-    blobObjects.push(circle);
   }
 }
 
 function moveBlobs() {
-  for (var i=0; i < blobObjects.length; i++) {
-    var circle = blobObjects[i];
-    var y = parseInt(circle.getAttribute("cy"), 10);
-    if (y == 600) {
+  for (var i=0; i < blobAnimations.length; i++) {
+    var animation = blobAnimations[i];
+    var y = parseInt(animation.parentElement.getAttribute("cy"), 10);
+    var oldY = y;
+    animation.setAttribute("calcMode", "linear");
+    if (y >= 599) {
       y = ((300 * Math.random()) >> 0) + 200;
-      circle.setAttribute("r", ((1 + Math.random() * 5) >> 0) * 5);
+      animation.parentElement.setAttribute("r", ((1 + Math.random() * 5) >> 0) * 5);
+      //animation.setAttribute("calcMode", "spline");
     }
     y += 10;
     if (y > 600)
       y = 600;
-    circle.setAttribute("cy", y);
+    animation.setAttribute("from", oldY);
+    animation.setAttribute("to", y);
+    animation.beginElement();
   }
 }
 
